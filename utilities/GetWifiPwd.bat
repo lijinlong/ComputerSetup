e8265779f093d7a4e5d673dce6fb0df108b9ee05
Li Jinlong <jinlong.li@noitom.com>
2023-08-16 13:08:12 +0800

    add small utility

---
+++ utilities/GetWifiPwd.bat
@@ -0,0 +1,8 @@
+﻿ @echo off
+ chcp 65001
+ FOR /F "usebackq tokens=2 delims=:" %%i IN (`netsh wlan show profiles`) DO (
+ 	@echo #Password of %%i#
+ 	netsh wlan show profiles name=%%i key=clear | findstr /I "key\ content"
+ 	@echo.
+ )
+ pause
